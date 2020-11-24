513106a9817b307c2787d160e684b6b746d776bb
DayraAlvarez <dayra.g.alvarez@gmail.com>
2020-11-24 01:32:00 -0600

    Adding bootstrap labels for mobile, pending for webapp

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDqnV43_CWIoEfXijUQhNYA6NyPhikwli8",
+    authDomain: "boniteces-9696b.firebaseapp.com",
+    databaseURL: "https://boniteces-9696b.firebaseio.com",
+    projectId: "boniteces-9696b",
+    storageBucket: "boniteces-9696b.appspot.com",
+    messagingSenderId: "248765570046",
+    appId: "1:248765570046:web:93acfb78594f07da2ebeaf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
